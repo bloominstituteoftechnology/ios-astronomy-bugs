b730fcce0a55e1eca8f1f9e39d487fdc5b5f8e68
Andrew Madsen <andrew@openreelsoftware.com>
2018-09-09 19:32:29 -0600

    More 🐛🐛🐛

--- Astronomy/Model/Cache.swift
+++ Astronomy/Model/Cache.swift
@@ -11,15 +11,12 @@ import UIKit
 class Cache<Key: Hashable, Value> {
     
     func cache(value: Value, for key: Key) {
-        queue.async {
-            self.cache[key] = value
-        }
+        cache[key] = value
     }
     
     func value(for key: Key) -> Value? {
-        return queue.sync { cache[key] }
+        return cache[key]
     }
     
     private var cache = [Key : Value]()
-    private let queue = DispatchQueue(label: "com.LambdaSchool.Astronomy.CacheQueue")
 }
--- Astronomy/View Controllers/PhotoDetailViewController.swift
+++ Astronomy/View Controllers/PhotoDetailViewController.swift
@@ -33,7 +33,7 @@ class PhotoDetailViewController: UIViewController {
     private func updateViews() {
         guard let photo = photo, isViewLoaded else { return }
         do {
-            let data = try Data(contentsOf: photo.imageURL.usingHTTPS!)
+            let data = try Data(contentsOf: photo.imageURL)
             imageView.image = UIImage(data: data)
             let dateString = dateFormatter.string(from: photo.earthDate)
             detailLabel.text = "Taken by \(photo.camera.roverId) on \(dateString) (Sol \(photo.sol))"
